0f4fefc01a9ab0088f06ff3828611c7185545e2e
Azizah Lameed <lameed.azizah@gmail.com>
2026-02-08 10:13:14 -0500

    Update matching page

--- src/main/frontend/src/Pages/Matching.jsx
+++ src/main/frontend/src/Pages/Matching.jsx
@@ -11,12 +11,25 @@ function Matching() {
     return (
         <>
             <Link to="/"></Link>
-            <h1>MATCHITY</h1>
-            <h2>Here are your options: </h2>
+            <h1 class="title">MATCHITY</h1>
+            <h2 class="subtitle">Here are your best matches</h2>
 
-            {/* <div id="charities">
-                <Charity></Charity>
-            </div> */}
+            <dialog id="charities" class="card">
+                <div class="card-content">
+                    <h3 id="name"></h3>
+                    <p id="description"></p>
+                    <span id="urgency" class="urgency-badge"></span>
+                </div>
+
+                <div class="actions">
+                    <button id="noButtonLink" class="btn no">No</button>
+                    <button id="yesButtonLink" class="btn yes">Yes</button>
+                </div>
+
+                <button id="finalizeLink" class="finalize">
+                    Finalize matching
+                </button>
+            </dialog>
             <button type="button" id="finalizeLink" onClick={ () => { navigate('/results')} }> Finalize matching </button>
         </>
     )
